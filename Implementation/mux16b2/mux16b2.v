bd031a987fabad9ec80727f2f65e055954919714
alfayeka <alfayeka@008d8755-5fb0-4236-8a45-150205e6b5c8>
2017-02-01 04:26:24 +0000

    muxes complete?

---
+++ Implementation/mux16b2/mux16b2.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:20:43 01/31/2017 
+// Design Name: 
+// Module Name:    mux16b2 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mux16b2(
+    input [15:0] in0,
+    input [15:0] in1,
+    output reg [15:0] out,
+    input set
+    );
+
+	always @ (*)
+		begin
+		
+			case(set)
+			
+				0:
+					assign out = in0;
+				1:
+					assign out = in1;
+				default:
+					$display("ERROR! SET VALUE OUT OF RANGE!");
+				
+			endcase
+			
+		end
+
+endmodule
