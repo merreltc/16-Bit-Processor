8624434be31a63925cfcb418562635d70f6bfbd3
merreltc <merreltc@008d8755-5fb0-4236-8a45-150205e6b5c8>
2017-02-07 02:02:06 +0000

    Cleanup up folder

--- Implementation/mux16b4/work/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/alfayeka/Desktop/HW and shit/CSSE/CSSE232/mux1b/work/mux3b_test_isim_beh.exe" -prj "C:/Users/alfayeka/Desktop/HW and shit/CSSE/CSSE232/mux1b/work/mux3b_test_beh.prj" "work.mux3b_test" "work.glbl" 
