e30a0a3c1b4c51cbbd20e6a0261ca3d9929db150
Trinity Merrell <merreltc@rose-hulman.edu>
2017-02-08 01:53:54 +0000

    Added necessary files

---
+++ Implementation/datapath/m16b2_1.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:17:59 01/30/2017 
+// Design Name: 
+// Module Name:    mux2b 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module m16b2_1(
+    input [15:0] I0,
+    input [15:0] I1,
+    input [1:0] S,
+    output reg [3:0] O
+    );
+	
+	always @ (*)
+		begin
+		
+			case(S)
+			
+				0:
+					assign O = I0;
+				1:
+					assign O = I1;
+				default:
+					$display("ERROR! SET VALUE OUT OF RANGE!");
+				
+			endcase
+			
+		end
+
+
+endmodule
